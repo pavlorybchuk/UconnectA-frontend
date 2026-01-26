7448f79598267476a0092a5bf101729c55006166
Pavlo <pablorybcuk@gmail.com>
2026-01-26 09:07:03 +0200

    First launch

---
+++ android/app/src/main/kotlin/com/example/uconnecta/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.uconnecta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainUconnectA());
+}
+
+class MainUconnectA extends StatelessWidget {
+  const MainUconnectA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
